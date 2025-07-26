bb3fcbce1103248758af47c43db0b6b039fe84d1
TamarEngel <tamar05861@gmail.com>
2025-07-27 00:29:30 +0300

    Add video cutting feature using ffmpeg

--- FFmpeg.Infrastructure/Services/FFmpegServiceFactory.cs
+++ FFmpeg.Infrastructure/Services/FFmpegServiceFactory.cs
@@ -14,6 +14,7 @@ namespace FFmpeg.Infrastructure.Services
     public interface IFFmpegServiceFactory
     {
         ICommand<WatermarkModel> CreateWatermarkCommand();
+        ICommand<VideoCuttingModel> CreateVideoCuttingCommand();
     }
 
     public class FFmpegServiceFactory : IFFmpegServiceFactory
@@ -35,6 +36,11 @@ namespace FFmpeg.Infrastructure.Services
         public ICommand<WatermarkModel> CreateWatermarkCommand()
         {
             return new WatermarkCommand(_executor, _commandBuilder);
+        }
+
+        public ICommand<VideoCuttingModel> CreateVideoCuttingCommand()
+        {
+            return new VideoCuttingCommand(_executor, _commandBuilder);
         }
     }
 }
