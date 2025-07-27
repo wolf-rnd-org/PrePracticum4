45ff469d257bba39ff7d994e462634b286c13a3e
rachelyLevisson <el8567510@gmail.com>
2025-07-28 02:49:42 +0300

    fixed the other code

--- FFmpeg.Infrastructure/Commands/VideoCompressionCommand.cs
+++ FFmpeg.Infrastructure/Commands/VideoCompressionCommand.cs
@@ -1,33 +1,33 @@
-﻿//using Ffmpeg.Command.Commands;
-//using FFmpeg.Core.Models;
-//using FFmpeg.Infrastructure.Services;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using Ffmpeg.Command.Commands;
+using FFmpeg.Core.Models;
+using FFmpeg.Infrastructure.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace FFmpeg.Infrastructure.Commands
-//{
-//    public  class VideoCompressionCommand : BaseCommand, ICommand<VideoCompreesinModel>
-//    {
-//        private readonly ICommandBuilder _commandBuilder;
+namespace FFmpeg.Infrastructure.Commands
+{
+    public class VideoCompressionCommand : BaseCommand, ICommand<VideoCompreesinModel>
+    {
+        private readonly ICommandBuilder _commandBuilder;
 
-//        public VideoCompressionCommand(FFmpegExecutor executor, ICommandBuilder commandBuilder)
-//            : base(executor)
-//        {
-//            _commandBuilder = commandBuilder ?? throw new ArgumentNullException(nameof(commandBuilder));
-//        }
+        public VideoCompressionCommand(FFmpegExecutor executor, ICommandBuilder commandBuilder)
+            : base(executor)
+        {
+            _commandBuilder = commandBuilder ?? throw new ArgumentNullException(nameof(commandBuilder));
+        }
 
-//        public async Task<CommandResult> ExecuteAsync(VideoCompreesinModel model)
-//        {
-//            CommandBuilder = _commandBuilder
-//            .SetInput(model.InputFile)
-//            .SetVideoCodec("libx264")
-//            .AddOption("-crf 28") // איכות נמוכה יותר = קובץ קטן יותר
-//            .SetOutput(model.OutputFile);
+        public async Task<CommandResult> ExecuteAsync(VideoCompreesinModel model)
+        {
+            CommandBuilder = _commandBuilder
+            .SetInput(model.InputFile)
+            .SetVideoCodec("libx264")
+            .AddOption("-crf 28") // איכות נמוכה יותר = קובץ קטן יותר
+            .SetOutput(model.OutputFile);
 
-//            return await RunAsync();
-//        }
-//    }
-//}
+            return await RunAsync();
+        }
+    }
+}
