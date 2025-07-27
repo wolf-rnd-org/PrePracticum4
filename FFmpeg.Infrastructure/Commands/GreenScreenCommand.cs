0c5d6795826ebc27942066900397fa427c64efca
Yael2151 <yael215128@gmail.com>
2025-07-27 15:42:07 +0300

    Add GreenScreenModel and GreenScreenCommand for green screen video processing

---
+++ FFmpeg.Infrastructure/Commands/GreenScreenCommand.cs
@@ -0,0 +1,40 @@
+﻿using FFmpeg.Core.Models;
+using FFmpeg.Infrastructure.Commands;
+using FFmpeg.Infrastructure.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace FFmpeg.Infrastructure.Commands
+namespace Ffmpeg.Command.Commands
+{
+    public class GreenScreenCommand : BaseCommand, ICommand<GreenScreenModel>
+    {
+        private readonly ICommandBuilder _commandBuilder;
+
+        public GreenScreenCommand(FFmpegExecutor executor, ICommandBuilder commandBuilder)
+            : base(executor)
+        {
+            _commandBuilder = commandBuilder ?? throw new ArgumentNullException(nameof(commandBuilder));
+        }
+
+        public async Task<CommandResult> ExecuteAsync(GreenScreenModel model)
+        {
+            // יצירת הפילטר לפי הנתונים שקיבלנו
+            string filterExpression = $"[0:v]chromakey={model.ChromaColor}:{model.Similarity}:{model.Blend}[ckout];[1:v][ckout]overlay[out]";
+
+            CommandBuilder = _commandBuilder
+                .SetInput(model.InputFile)
+                .SetInput(model.BackgroundFile)
+                .AddFilterComplex(filterExpression)
+                .SetVideoCodec(model.VideoCodec)
+                .AddOption("-map 0:a?") // שמירת אודיו אם יש
+                .AddOption("-c:a copy")
+                .SetOutput(model.OutputFile);
+
+            return await RunAsync();
+        }
+    }
+}
