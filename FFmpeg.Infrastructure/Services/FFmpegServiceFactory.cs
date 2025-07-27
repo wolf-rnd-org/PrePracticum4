9e3f2b3b779217f25290bca4c8f218c4cd5cb9f8
Efrat-Lavi <efratl3164@gmail.com>
2025-07-27 20:50:03 +0300

    final

--- FFmpeg.Infrastructure/Services/FFmpegServiceFactory.cs
+++ FFmpeg.Infrastructure/Services/FFmpegServiceFactory.cs
@@ -14,6 +14,7 @@ namespace FFmpeg.Infrastructure.Services
     public interface IFFmpegServiceFactory
     {
         ICommand<WatermarkModel> CreateWatermarkCommand();
+        ICommand<ConvertAudioModel> CreateConvertAudioCommand();
     }
 
     public class FFmpegServiceFactory : IFFmpegServiceFactory
@@ -35,6 +36,10 @@ namespace FFmpeg.Infrastructure.Services
         public ICommand<WatermarkModel> CreateWatermarkCommand()
         {
             return new WatermarkCommand(_executor, _commandBuilder);
+        }
+        public ICommand<ConvertAudioModel> CreateConvertAudioCommand()
+        {
+            return new ConvertAudioCommand(_executor, _commandBuilder);
         }
     }
 }
