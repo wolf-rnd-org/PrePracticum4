179ad3d6beff968589b729855da8c64fbee1c5bb
rachelyLevisson <el8567510@gmail.com>
2025-07-29 00:33:37 +0300

    Update VideoEndpoints.cs

--- Ffmpeg.API/Endpoints/VideoEndpoints.cs
+++ Ffmpeg.API/Endpoints/VideoEndpoints.cs
@@ -20,12 +20,10 @@ namespace FFmpeg.API.Endpoints
         {
             app.MapPost("/api/video/watermark", AddWatermark)
                 .DisableAntiforgery()
-                .WithMetadata(new RequestSizeLimitAttribute(104857600)); // 100 MB
                 .WithMetadata(new RequestSizeLimitAttribute(MaxUploadSize)); // 100 MB
 
             app.MapPost("/api/video/rotation", AddRotation)
                 .DisableAntiforgery()
-                .WithMetadata(new RequestSizeLimitAttribute(104857600)); // 100 MB
                 .WithMetadata(new RequestSizeLimitAttribute(MaxUploadSize));
         }
 
