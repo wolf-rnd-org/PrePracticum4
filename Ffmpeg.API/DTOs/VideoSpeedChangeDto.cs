7087c9aeac12029154ed2b5dead841bfff27a3bf
chayaziv <chy15658@gmail.com>
2025-07-29 02:22:06 +0300

    add line

--- Ffmpeg.API/DTOs/VideoSpeedChangeDto.cs
+++ Ffmpeg.API/DTOs/VideoSpeedChangeDto.cs
@@ -2,10 +2,11 @@
 {
     public class VideoSpeedChangeDto
     {
-            public IFormFile VideoFile { get; set; }
+        public IFormFile VideoFile { get; set; }
 
-            public double Speed{ get; set; } 
+        public double Speed { get; set; }
 
-            public string OutputFileName { get; set; }
+        public string OutputFileName { get; set; }
     }
 }
+
