636abf12a054791541f79a1a8b34cb27a2c572f2
rachelyLevisson <el8567510@gmail.com>
2025-07-29 22:49:50 +0300

    nothin conflict

--- FFmpeg.Core/Models/VideoCompressionModel.cs
+++ FFmpeg.Core/Models/VideoCompressionModel.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace FFmpeg.Core.Models
 {
-<<<<<<< HEAD:FFmpeg.Core/Models/VideoCompreesinModel.cs
-    public class VideoCompreesinModel
-=======
     public class VideoCompressionModel
->>>>>>> a7e3064903a680ec3734cfa1471f04426dead63c:FFmpeg.Core/Models/VideoCompressionModel.cs
     {
         public string InputFile { get; set; }
         public string OutputFile { get; set; }
